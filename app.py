ac2ce747c81e716cacd4a21938c7a912fa23ae8c
marphlap <dlawngnome@gmail.com>
2020-05-07 03:35:11 -0400

    testing git actions

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ import dash
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets, url_base_pathname='/')
+app = dash.Dash(__name__,
+                external_stylesheets=external_stylesheets,
+                url_base_pathname='/')
 server = app.server
 app.config.suppress_callback_exceptions = True
 app.scripts.config.serve_locally = False
